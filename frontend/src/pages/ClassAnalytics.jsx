2fe634ab2b3f89b87efd36c9b6ccebe587ba3658
NatiqueAsghar <itsnatique786@gmail.com>
2025-11-22 23:51:57 +0530

    update api

--- frontend/src/pages/ClassAnalytics.jsx
+++ frontend/src/pages/ClassAnalytics.jsx
@@ -14,7 +14,7 @@ function ClassAnalytics() {
 
   const fetchData = async () => {
     try {
-      const res = await api.get(`/class/getByName/${name}`);
+      const res = await api.get(`/api/class/getByName/${name}`);
       setClassData(res.data);
     } catch (error) {
       console.error("Error fetching data:", error);
